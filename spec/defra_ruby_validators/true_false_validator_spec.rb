bde45771e46f9f774f12f083c43b1db92a8ef056
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-02-12 11:14:35 +0000

    Replace dummy class with Struct
    Using a struct provides a lighter weight implementation for testing our validators with the tests.

--- spec/defra_ruby_validators/true_false_validator_spec.rb
+++ spec/defra_ruby_validators/true_false_validator_spec.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
+module Test
+  TrueFalseValidatable = Struct.new(:value) do
+    include ActiveModel::Validations
+
+    validates :value, "defra_ruby_validators/true_false": true
+  end
+end
+
 module DefraRubyValidators
-  RSpec.describe TrueFalseValidator do
+  RSpec.describe TrueFalseValidator, type: :model do
+
     %w[true false].each do |value|
       context "when given the valid value '#{value}'" do
+        subject { Test::TrueFalseValidatable.new(value) }
+
         it "confirms the value is valid" do
-          expect(DummyTrueFalse.new(value)).to be_valid
+          expect(subject).to be_valid
         end
       end
     end
 
     context "when given an invalid value" do
       context "because it is blank" do
-        subject { DummyTrueFalse.new("") }
+        subject { Test::TrueFalseValidatable.new("") }
 
         it "confirms the value is not valid" do
           expect(subject).to_not be_valid
@@ -26,34 +37,22 @@ module DefraRubyValidators
           expect(subject.errors[:value][0]).to eq(I18n.t("defra_ruby_validators.true_false.errors.blank"))
         end
       end
-    end
 
-    context "because the value is not recognised" do
-      subject { DummyTrueFalse.new("foobar") }
+      context "because the value is not recognised" do
+        subject { Test::TrueFalseValidatable.new("foobar") }
 
-      it "confirms the value is not valid" do
-        expect(subject).to_not be_valid
+        it "confirms the value is not valid" do
+          expect(subject).to_not be_valid
+        end
+        it "adds an error to the subject's errors collection" do
+          subject.valid?
+          expect(subject.errors.count).to eq(1)
+        end
+        it "adds an error with the correct message" do
+          subject.valid?
+          expect(subject.errors[:value][0]).to eq(I18n.t("defra_ruby_validators.true_false.errors.invalid"))
+        end
       end
-      it "adds an error to the subject's errors collection" do
-        subject.valid?
-        expect(subject.errors.count).to eq(1)
-      end
-      it "adds an error with the correct message" do
-        subject.valid?
-        expect(subject.errors[:value][0]).to eq(I18n.t("defra_ruby_validators.true_false.errors.invalid"))
-      end
-    end
-
-    class DummyTrueFalse
-      include ActiveModel::Model
-
-      attr_accessor :value
-
-      def initialize(value)
-        @value = value
-      end
-
-      validates :value, "defra_ruby_validators/true_false": true
     end
   end
 end
