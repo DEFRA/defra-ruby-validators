ef36fcdf4892729b3156b164646090b9261fcd5e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-05-13 20:21:29 +0100

    Move dotenv into support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,6 @@ require "bundler/setup"
 # This is as per its docs https://github.com/colszowka/simplecov#getting-started
 require "./spec/support/simplecov"
 
-# Load env vars from a text file
-require "dotenv/load"
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
