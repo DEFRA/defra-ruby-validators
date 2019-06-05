28842a109e9d345f37eb4273e62f7b15df1bba08
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-06-06 00:04:48 +0100

    Add token validator
    This is essentially a copy and paste of the validator from [waste-exemptions-engine](https://github.com/DEFRA/waste-exemptions-engine).
    
    Adding it here means we can begin to share the validator across our services.

---
+++ spec/defra_ruby/validators/token_validator_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Test
+  TokenValidatable = Struct.new(:token) do
+    include ActiveModel::Validations
+
+    validates :token, "defra_ruby/validators/token": true
+  end
+end
+
+module DefraRuby
+  module Validators
+    RSpec.describe TokenValidator, type: :model do
+
+      invalid_token = "123456"
+
+      it_behaves_like "a validator"
+      it_behaves_like "a presence validator", TokenValidator, Test::TokenValidatable, :token
+
+      describe "#validate_each" do
+        context "when the token is not valid" do
+          context "because the token is not correctly formatted" do
+            validatable = Test::TokenValidatable.new(invalid_token)
+            error_message = Helpers::Translator.error_message(klass: TokenValidator, error: :invalid_format)
+
+            it_behaves_like "an invalid record", validatable, :token, error_message
+          end
+        end
+      end
+    end
+  end
+end
