d4c66e0990f57d6d35aa6ddd477ed028089b83b5
irisfaraway <iris.faraway@gmail.com>
2019-08-06 11:20:47 +0100

    Use options[:message] if provided
    This should let us pass in a custom message param when we use the validator in a model.

--- spec/support/shared_examples/validators/invalid_record.rb
+++ spec/support/shared_examples/validators/invalid_record.rb
@@ -15,4 +15,14 @@ RSpec.shared_examples "an invalid record" do |validatable, attribute, error_mess
     expect(error_message).to_not include("translation missing:")
     expect(validatable.errors[attribute]).to eq([error_message])
   end
+
+  context "when there is a custom error message" do
+    let(:custom_message) { "something is wrong (in a customised way)" }
+    before { allow_any_instance_of(DefraRuby::Validators::BaseValidator).to receive(:options).and_return(message: custom_message) }
+
+    it "uses the custom message instead of the default" do
+      validatable.valid?
+      expect(validatable.errors[attribute]).to eq([custom_message])
+    end
+  end
 end
