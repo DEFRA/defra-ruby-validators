3379bffbae22dc2128057abf734a2456610fb182
irisfaraway <iris.faraway@gmail.com>
2019-08-20 15:19:38 +0100

    Add option to validate 'overseas' as location
    This is required if we're going to use this validator in the waste-carriers-engine. However, the default is to not include "overseas".

--- lib/defra_ruby/validators/location_validator.rb
+++ lib/defra_ruby/validators/location_validator.rb
@@ -6,9 +6,20 @@ module DefraRuby
       include CanValidateSelection
 
       def validate_each(record, _attribute, value)
-        valid_options = %w[england northern_ireland scotland wales]
-
         value_is_included?(record, :location, value, valid_options)
+      end
+
+      private
+
+      def valid_options
+        options = %w[england northern_ireland scotland wales]
+        options.push("overseas") if allow_overseas?
+
+        options
+      end
+
+      def allow_overseas?
+        options[:allow_overseas] == true
       end
     end
   end
--- spec/defra_ruby/validators/location_validator_spec.rb
+++ spec/defra_ruby/validators/location_validator_spec.rb
@@ -25,6 +25,32 @@ module DefraRuby
         :location,
         valid: valid_location, invalid: invalid_location
       )
+
+      context "when the value is overseas" do
+        validatable = Test::LocationValidatable.new("overseas")
+
+        context "when overseas_allowed is enabled" do
+          before do
+            allow_any_instance_of(DefraRuby::Validators::BaseValidator).to receive(:options).and_return(allow_overseas: true)
+          end
+
+          it_behaves_like "a valid record", validatable
+        end
+
+        context "when overseas_allowed is not enabled" do
+          before do
+            allow_any_instance_of(DefraRuby::Validators::BaseValidator).to receive(:options).and_return({})
+          end
+
+          error_message = Helpers::Translator.error_message(LocationValidator, :location, :inclusion)
+
+          it_behaves_like "an invalid record",
+                          validatable: validatable,
+                          attribute: :location,
+                          error: :inclusion,
+                          error_message: error_message
+        end
+      end
     end
   end
 end
