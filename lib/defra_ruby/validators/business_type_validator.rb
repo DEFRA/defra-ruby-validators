dd69b16d98d89b6c714aade7f2e3cc9d21789f23
irisfaraway <iris.faraway@gmail.com>
2019-08-20 15:19:38 +0100

    Add option to validate 'overseas' as business type
    This is required if we're going to use this validator in the waste-carriers-engine. However, the default is to not include "overseas".

--- lib/defra_ruby/validators/business_type_validator.rb
+++ lib/defra_ruby/validators/business_type_validator.rb
@@ -6,9 +6,20 @@ module DefraRuby
       include CanValidateSelection
 
       def validate_each(record, _attribute, value)
-        valid_options = %w[soleTrader limitedCompany partnership limitedLiabilityPartnership localAuthority charity]
-
         value_is_included?(record, :business_type, value, valid_options)
+      end
+
+      private
+
+      def valid_options
+        options = %w[soleTrader limitedCompany partnership limitedLiabilityPartnership localAuthority charity]
+        options.push("overseas") if allow_overseas?
+
+        options
+      end
+
+      def allow_overseas?
+        options[:allow_overseas] == true
       end
     end
   end
