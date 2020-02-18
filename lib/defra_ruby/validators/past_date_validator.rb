2e4a5cf6f424416d36d50a6966181668e97294c3
cintamani <cintamani.puddu@gmail.com>
2020-02-18 14:58:57 +0000

    Update past_date_validator.rb

--- lib/defra_ruby/validators/past_date_validator.rb
+++ lib/defra_ruby/validators/past_date_validator.rb
@@ -5,11 +5,7 @@ module DefraRuby
     class PastDateValidator < BaseValidator
       def validate_each(record, attribute, value)
         return false if value.blank?
-        # value_is_included?(record, attribute, value, valid_options)
 
-        # if value < other_date
-        #   record.errors.add(attribute, (options[:message] || "must be after #{options[:with].to_s.humanize}"))
-        # end
         return true if value.to_date <= Date.today
 
         record.errors[attribute] << error_message(:past_date)
