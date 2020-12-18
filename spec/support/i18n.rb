9d7fc193d24d9cd71f7df742182ae4de7a51da95
Iris Faraway <iris.faraway@gmail.com>
2020-12-18 16:46:59 +0000

    Switch from Travis CI to GitHub Actions (#61)
    https://eaflood.atlassian.net/browse/RUBY-1229
    
    We have decided to switch from Travis CI to GitHub Actions. This PR updates our config and readme.
    
    * Fix style issues, remove defunct docs

--- spec/support/i18n.rb
+++ spec/support/i18n.rb
@@ -4,5 +4,5 @@
 # gem in a rails app to confirm these are being pulled through correctly.)
 require "i18n"
 
-I18n.load_path << Dir[File.expand_path("config/locales") + "/**/*.yml"]
+I18n.load_path << Dir["#{File.expand_path('config/locales')}/**/*.yml"]
 I18n.default_locale = :en
